4d048db4b94a238add249bb1b14bcf6c7814020a
madebyrael <israelxzz7@gmail.com>
2026-08-20 20:30:23 -0300

    exercício if-else

---
+++ Condicionais/IfElse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IfElse{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+int number = scanner.nextInt();
+        if(number % 2 != 0){
+            System.out.println("Weird");
+        } else if(number % 2 == 0 && number >= 6 && number <= 20){
+        System.out.println("Weird");
+    } else if(number % 2 == 0 && number >= 2 && number <= 5){
+        System.out.println("Not Weird");
+    } else if(number % 2 == 0 && number > 20){
+        System.out.println("Not Weird");
+    }
+}
+}
