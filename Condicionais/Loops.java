a677f42c8a074352a62a8c97bcf5f335970b3663
madebyrael <israelxzz7@gmail.com>
2026-08-20 15:58:07 -0300

    exercício break

---
+++ Condicionais/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
